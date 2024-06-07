d07b7a283ba8e354aa9f320db9d459204e1ccc67
tanneberger <revol-xut@protonmail.com>
2024-06-07 15:38:19 +0200

    maybe fixing cmake

--- mqtt.cmake
+++ mqtt.cmake
@@ -1,30 +1,5 @@
-# find_package(Mosquitto REQUIRED)x
-list(APPEND CMAKE_PREFIX_PATH "/opt/homebrew/")
-list(APPEND CMAKE_PREFIX_PATH "/usr/local/")
+# https://github.com/eclipse/paho.mqtt.c?tab=readme-ov-file#building-your-application-with-cmake
 
-find_package(OpenSSL)
-if(${OpenSSL_FOUND})
-    set(PAHO_WITH_SSL true)
-ELSE (${OpenSSL_FOUND})
-    set(PAHO_WITH_SSL false)
-    MESSAGE(ERROR " Could not locate OpenSSL. Will skip using it.")
-ENDIF (${OpenSSL_FOUND})
-# Taken from: https://github.com/eclipse/paho.mqtt.cpp/tree/master/cmake
-# find the Paho MQTT C library
-if(PAHO_WITH_SSL)
-    set(_PAHO_MQTT_C_LIB_NAME paho-mqtt3cs)
-else()
-    set(_PAHO_MQTT_C_LIB_NAME paho-mqtt3c)
-endif()
+find_package(eclipse-paho-mqtt-c REQUIRED)
 
-# add suffix when using static Paho MQTT C library variant on Windows
-if(WIN32)
-    if(PAHO_BUILD_STATIC)
-        set(_PAHO_MQTT_C_LIB_NAME ${_PAHO_MQTT_C_LIB_NAME}-static)
-    endif()
-endif()
-
-find_library(PAHO_MQTT_C_LIBRARIES NAMES ${_PAHO_MQTT_C_LIB_NAME})
-
-include_directories(${PAHO_MQTT_C_INCLUDE_DIRS})
-target_link_libraries(${LF_MAIN_TARGET} PUBLIC ${PAHO_MQTT_C_LIB})
+target_link_libraries(${LF_MAIN_TARGET} PUBLIC eclipse-paho-mqtt-c::paho-mqtt3a) 
